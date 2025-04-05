2b20a643489604ac842965a9630f9078c31f7e51
Jared <f7.z7ro@gmail.com>
2025-04-05 14:39:35 -0600

    3rd commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.DEPLOY_BASE_URL,
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  base: '/pokedex/',
+  plugins: [react(), tailwindcss()],
+});
